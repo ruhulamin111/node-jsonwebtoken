80c59c9e40ecbf8d374736a5d03f8be73152c698
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-12 15:43:53 +0600

    jsonwebtoken

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
 
 app.use(cors())
 app.use(express.json())
