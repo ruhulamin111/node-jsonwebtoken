708433c0cc4f28902b518412ce315bc2b88ad4f6
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-12 14:15:59 +0600

    start json web token setup'

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express()
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+require('dotenv').config()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) =>
+    res.send('jsonwebtoken server')
+)
+app.listen(port, () => {
+    console.log('jsonwebtoken server running');
+})
+
+
