547f189eb3a2599e0d7dd5fd2b17c49534eddddc
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-12 19:47:57 +0600

    database connect

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -15,4 +16,28 @@ app.listen(port, () => {
     console.log('jsonwebtoken server running');
 })
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zjrcntk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        await client.connect()
+        const tokenCollection = client.db('webtoken').collection('token')
+
+        app.get('/products', async (req, res) => {
+            const result = await tokenCollection.find({}).toArray()
+            res.send(result)
+        })
+
+
+
+    }
+    catch (error) {
+        console.log(error);
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.dir)
